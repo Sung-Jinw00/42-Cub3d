850d6fb16f208f516e5260240fb6fdacbbeaa2b9
Louis Cagnion <locagnio@c1r1p6.42perpignan.fr>
2025-04-19 19:08:51 +0200

    includes added

---
+++ cub3d.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: locagnio <locagnio@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/19 19:01:51 by locagnio          #+#    #+#             */
+/*   Updated: 2025/04/19 19:06:40 by locagnio         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <math.h>
+# include <fcntl.h> // open
+# include <stdio.h> // printf, perror
+# include <unistd.h> // read, write, close
+# include <stdlib.h> // malloc, free, exit
+# include <string.h> // strerror
+
+# include "ultimate_libft.h"
+
+#endif
